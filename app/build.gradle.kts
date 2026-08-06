bfa5620545c5f8df703370f28cea7cc9f032964d
Kissie Ann Apple C. Aput <kaaput42160@liceo.edu.ph>
2026-08-06 13:48:26 +0800

    lab activity 6

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,4 +54,4 @@ dependencies {
     debugImplementation(libs.androidx.compose.ui.test.manifest)
     debugImplementation(libs.androidx.compose.ui.tooling)
     implementation("androidx.compose.material:material-icons-extended")
-}
+}   
